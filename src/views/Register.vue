fb764693131d5b319b4a451e9324fe9c466271bd
esakimedia <apachecordovax@gmail.com>
2022-03-19 21:34:06 +0100

    register, login and logout user

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -15,7 +15,7 @@
             >login to your account</router-link>
         </p>
     </div>
-    <form class="mt-8 space-y-6" action="#" method="POST">
+    <form class="mt-8 space-y-6" @submit="register">
         <input type="hidden" name="remember" value="true" />
         <div class="rounded-md shadow-sm -space-y-px">
             <div>
@@ -24,10 +24,11 @@
                     id="fullname"
                     name="name"
                     type="text"
-                    autocomplete="email"
+                    autocomplete="name"
                     required
                     class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                     placeholder="Full Name"
+                    v-model="user.name"
                 />
             </div>
             <div>
@@ -40,6 +41,7 @@
                     required
                     class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                     placeholder="Email address"
+                    v-model="user.email"
                 />
             </div>
             <div>
@@ -50,8 +52,22 @@
                     type="password"
                     autocomplete="current-password"
                     required
-                    class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                    class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                     placeholder="Password"
+                    v-model="user.password"
+                />
+            </div>
+            <div>
+                <label for="password_confirmation" class="sr-only">Password Confirmation</label>
+                <input
+                    id="password_confirmation"
+                    name="password_confirmation"
+                    type="password"
+                    autocomplete="current-password_confirmation"
+                    required
+                    class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                    placeholder="Password Confirmation"
+                    v-model="user.password_confirmation"
                 />
             </div>
         </div>
@@ -73,12 +89,28 @@
     </form>
 </template>
 
-<script>
+<script setup>
 import { LockClosedIcon } from '@heroicons/vue/solid'
+import { useRouter } from 'vue-router';
+import store from '../store';
 
-export default {
-    components: {
-        LockClosedIcon,
-    },
+const router = useRouter();
+
+const user = {
+    name: '',
+    email: '',
+    password: '',
+    password_confirmation: ''
+}
+
+function register(ev) {
+    ev.preventDefault();
+    store
+        .dispatch('register', user)
+        .then((res) => {
+            router.push({
+                name: 'Dashboard'
+            })
+        })
 }
 </script>
