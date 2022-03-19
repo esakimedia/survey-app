34d1eff24578a0b696d7a2d86a5c88460bc46dcb
esakimedia <apachecordovax@gmail.com>
2022-03-19 10:30:10 +0100

    add login, register, dashboard and surveys pages

--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
 import { createApp } from 'vue'
+import store from './store'
 import App from './App.vue'
+import './index.css'
+import router from './router'
 
-createApp(App).mount('#app')
+createApp(App)
+    .use(store)
+    .use(router)
+    .mount('#app')
