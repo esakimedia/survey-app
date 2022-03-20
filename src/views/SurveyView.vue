d36020898460f9a9666b778f3746e8c50bd37af3
esakimedia <apachecordovax@gmail.com>
2022-03-20 19:34:43 +0100

    save and update survey

--- src/views/SurveyView.vue
+++ src/views/SurveyView.vue
@@ -159,7 +159,7 @@
 
                 <div class="px-4 py-3 bg-gray-50 text-right sm:px-6">
                     <button
-                        type="button"
+                        type="submit"
                         class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                     >Save</button>
                 </div>
@@ -171,12 +171,13 @@
 <script setup>
 import { v4 as uuidv4 } from "uuid";
 import { ref } from 'vue';
-import { useRoute } from 'vue-router';
+import { useRoute, useRouter } from 'vue-router';
 import PageComponent from '../components/PageComponent.vue';
 import QuestionEditor from '../components/editor/QuestionEditor.vue';
 import store from '../store';
 
 const route = useRoute();
+const router = useRouter();
 
 // Create empty survey
 let model = ref({
@@ -219,6 +220,19 @@ function questionChange(question) {
         }
         return q;
     });
+}
+
+/**
+*   Create or update survey
+*/
+function saveSurvey() {
+    store.dispatch("saveSurvey", model.value)
+        .then(({ data }) => {
+            router.push({
+                name: "SurveyView",
+                params: { id: data.data.id },
+            });
+        });
 }
 </script>
 
